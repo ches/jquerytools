4734e9eb8711a94ef7d41bdcce1e7b8eb76f0141
Brad Robertson <bradleyrobertson@gmail.com>
2011-08-11 01:08:51 -0400

    fixed bug where animations would queue up when on a different tab in firefox/chrome
    fixes issue 455

--- src/scrollable/scrollable.autoscroll.js
+++ src/scrollable/scrollable.autoscroll.js
@@ -1,81 +1,88 @@
 /**
- * @license 
- * jQuery Tools @VERSION / Scrollable Autoscroll
- * 
- * NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
- * 
- * http://flowplayer.org/tools/scrollable/autoscroll.html
- *
- * Since: September 2009
- * Date: @DATE 
- */
-(function($) {		
+* @license 
+* jQuery Tools @VERSION / Scrollable Autoscroll
+* 
+* NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
+* 
+* http://flowplayer.org/tools/scrollable/autoscroll.html
+*
+* Since: September 2009
+* Date: @DATE 
+*/
+(function($) {
 
-	var t = $.tools.scrollable; 
-	
-	t.autoscroll = {
-		
-		conf: {
-			autoplay: true,
-			interval: 3000,
-			autopause: true
-		}
-	};	
-	
-	// jQuery plugin implementation
-	$.fn.autoscroll = function(conf) { 
+  var t = $.tools.scrollable;
 
-		if (typeof conf == 'number') {
-			conf = {interval: conf};	
-		}
-		
-		var opts = $.extend({}, t.autoscroll.conf, conf), ret;
-		
-		this.each(function() {		
-				
-			var api = $(this).data("scrollable");			
-			if (api) { ret = api; }
-			
-			// interval stuff
-			var timer, stopped = true;
-	
-			api.play = function() { 
-				
-				// do not start additional timer if already exists
-				if (timer) { return; }
-				
-				stopped = false;
-				
-				// construct new timer
-				timer = setInterval(function() { 
-					api.next();				
-				}, opts.interval);
-				
-			};	
+  t.autoscroll = {
 
-			api.pause = function() {
-				timer = clearInterval(timer);
-			};
-			
-			// when stopped - mouseover won't restart 
-			api.stop = function() {
-				api.pause();
-				stopped = true;	
-			};
-		
-			/* when mouse enters, autoscroll stops */
-			if (opts.autopause) {
-				api.getRoot().add(api.getNaviButtons()).hover(api.pause, api.play);
-			}
-			
-			if (opts.autoplay) {
-				api.play();				
-			}
+      conf: {
+          autoplay: true,
+          interval: 3000,
+          autopause: true
+      }
+  };
 
-		});
-		
-		return opts.api ? ret : this;
-		
-	}; 
-	
-})(jQuery);		
+  // jQuery plugin implementation
+  $.fn.autoscroll = function(conf) {
+
+    if (typeof conf == 'number') {
+      conf = {
+        interval: conf
+      };
+    }
+
+    var opts = $.extend({},
+    t.autoscroll.conf, conf),
+    ret;
+
+    this.each(function() {
+
+      var api = $(this).data("scrollable"),
+          root = api.getRoot(),
+          // interval stuff
+          timer;
+      
+      if (api) { ret = api; }
+
+      /**
+      *
+      *   Function to run autoscroll through event binding rather than setInterval
+      *   Fixes this bug: http://flowplayer.org/tools/forum/25/72029
+      */
+      function scroll(){        
+        timer = setTimeout(function(){
+          api.next();
+        }, opts.interval);
+      }
+      
+      api.play = function() {
+        root.bind('onSeek', scroll);
+        scroll();
+      };
+
+      api.pause = function() {
+        clearTimeout(timer);  // clear any queued items immediately
+        root.unbind('onSeek', scroll);
+      };
+
+      // when stopped - mouseover won't restart
+      api.stop = function() {
+        api.pause();
+      };
+
+      /* when mouse enters, autoscroll stops */
+      if (opts.autopause) {
+        api.getRoot().add(api.getNaviButtons()).hover(api.pause, api.play);
+      }
+
+      if (opts.autoplay) {
+        api.play();
+      }
+
+    });
+
+    return opts.api ? ret: this;
+
+  };
+
+})(jQuery);
